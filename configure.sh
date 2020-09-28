5d24dea8e1cdf05983cefdd82bf9d4cae57ebc5b
Eric Mikulin <ericm99@gmail.com>
2020-09-28 15:48:08 -0700

    Remove redundant ARM stuff

--- configure.sh
+++
@@ -1,5 +0,0 @@
-# UBCUAS
-# Maintainer: Eric Mikulin
-
-cp lib/* arm/
-cp lib/* x86/
---
+++ dockerxenv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Need to have experimental CLI and Engine (buildx + buildkit)
+export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_BUILDKIT=enabled
+
+# Add QEMU stuff
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# Create and bootstrap builder
+docker buildx create --name mubuilder
+docker buildx use mubuilder
+docker buildx inspect --bootstrap
