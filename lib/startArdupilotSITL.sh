63b45f2c48842e1b4a628c8f7d1c46bd4450e4a9
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-02-19 13:59:20 -0800

    Fix windows building fully

--- lib/startArdupilotSITL.sh
+++ lib/startArdupilotSITL.sh
@@ -32,38 +32,41 @@ arduPilotInstance=0
 if [ $numCopters -ne 0 ]; then
     for i in $(seq 0 $(($numCopters-1))); do
 
-           VEHICLE=arducopter
-           INSTANCE=$arduPilotInstance
+            VEHICLE=arducopter
+            INSTANCE=$arduPilotInstance
 
-           export SITL_RITW_TERMINAL="screen -D -m -S Copter${INSTANCE}"
+            export SITL_RITW_TERMINAL="screen -D -m -S Copter${INSTANCE}"
+            
+            rm -rf ./${VEHICLE}${INSTANCE}
+            mkdir ./${VEHICLE}${INSTANCE} && cd ${VEHICLE}${INSTANCE}
 
-           rm -rf /${VEHICLE}${INSTANCE}
-           mkdir /${VEHICLE}${INSTANCE} && cd /${VEHICLE}${INSTANCE}
+            simCommand="/copter/Tools/autotest/sim_vehicle.py \
+                -I${INSTANCE} \
+                --vehicle ArduCopter \
+                --custom-location=${LAT},${LON},${ALT},${DIR} \
+                -w \
+                --speedup ${SPEEDUP} \
+                --no-rebuild \
+                --no-mavproxy"
 
-           simCommand="/copter/Tools/autotest/sim_vehicle.py \
-              -I${INSTANCE} \
-              --vehicle ArduCopter \
-              --custom-location=${LAT},${LON},${ALT},${DIR} \
-              -w \
-              --speedup ${SPEEDUP} \
-              --no-rebuild \
-              --no-mavproxy"
+            echo "Starting Sim ${VEHICLE} with command '$simCommand'"
+            exec $simCommand &
+            pids[${arduPilotInstance}]=$!
 
-           echo "Starting Sim ${VEHICLE} with command '$simCommand'"
-           exec $simCommand &
-           pids[${arduPilotInstance}]=$!
+            #Make it so all the instances don't start at the same Lat/Lon
+            LAT=$(echo "$LAT + $incrementStepLat" | bc)
+            LON=$(echo "$LON + $incrementStepLon" | bc)
+            ALT=$(echo "$ALT + $incrementStepAlt" | bc)
+            HDG=$(echo "$HDG + $incrementStepHdg" | bc)
 
-           #Make it so all the instances don't start at the same Lat/Lon
-           LAT=$(echo "$LAT + $incrementStepLat" | bc)
-           LON=$(echo "$LON + $incrementStepLon" | bc)
-           ALT=$(echo "$ALT + $incrementStepAlt" | bc)
-           HDG=$(echo "$HDG + $incrementStepHdg" | bc)
+            # Exit folder
+            cd ..
 
-           # Increment arduPilotInstance
-           let arduPilotInstance=$(($arduPilotInstance+1))
+            # Increment arduPilotInstance
+            let arduPilotInstance=$(($arduPilotInstance+1))
 
-           # This shouldn't be necessary, but let's give it some time to spin-up
-           sleep 3
+            # This shouldn't be necessary, but let's give it some time to spin-up
+            sleep 3
     done
 fi
 
