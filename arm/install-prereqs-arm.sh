8e312fd3ae9e7726843afa3b4fe88b05f875048f
Elio Di Nino <52972357+ElioDiNino@users.noreply.github.com>
2023-02-20 20:25:10 -0800

    New arm attempt

--- arm/install-prereqs-arm.sh
+++ arm/install-prereqs-arm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Modified version of https://github.com/ArduPilot/ardupilot/blob/master/Tools/environment_install/install-prereqs-ubuntu.sh
 echo "---------- $0 start ----------"
 set -e
